5df14690fd07aba0f820fb35bc58ed160401e8de
Calvin Cramer <cjcramer@ucdavis.edu>
2019-04-24 23:49:25 -0700

    Cleaned up include files

--- lab1/lab1_merged/lane_line_detection/include/signal_proc.hpp
+++ lab1/lab1_merged/lane_line_detection/include/signal_proc.hpp
@@ -1,28 +1,16 @@
 #ifndef SIGNAL_PROC_HPP
 #define SIGNAL_PROC_HPP
 
-
+// Finds the beset fitting polynomial of order @order given a number of points
 void polyFit(const cv::Mat& src_x, const cv::Mat& src_y, cv::Mat &dst, int order);
 
+// Returns the values given a polynomial described by @best_fit from inputs in @src
 void polyVal(const cv::Mat& src, cv::Mat& dst, const cv::Mat& best_fit);
 
+// Linear step between start and stop with a certain number of points
 void linspace(double start, double stop, unsigned int num, cv::Mat& dst);
 
+// Returns the peaks of the histogram from a binary image
 std::vector<int> findPeaks(cv::Mat& binary_warped, cv::Mat& histogram);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif /* SIGNAL_PROC_HPP */
