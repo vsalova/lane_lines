64a7bfe3ca8aba779879e7372741c53314866747
Calvin Cramer <cjcramer@ucdavis.edu>
2019-05-02 10:28:56 -0700

    Added instruction file for culane

---
+++ lab1/lab1_hough/camera_calibration/build/CMakeFiles/intrinsics_tester.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intrinsics_tester.dir/src/intrinsics_tester.cpp.o"
+  "CMakeFiles/intrinsics_tester.dir/src/cv_helper.cpp.o"
+  "intrinsics_tester.pdb"
+  "intrinsics_tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intrinsics_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
